4aea2df853f168bc843bedf4d8b5ca1474f80be2
denzelr <ryan.denzel@colorado.edu>
2014-09-17 16:07:56 -0600

    update again

--- src/lab4/helloworld.java
+++ src/lab4/helloworld.java
@@ -4,5 +4,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display hello world
 		System.out.println("How are you?"); // Display courtesy
+		
+		
 	}
 }
